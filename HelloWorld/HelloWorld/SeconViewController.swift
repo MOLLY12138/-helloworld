750ed9c5b6a64b90743980b3583266f5addf4d15
MOLLY12138 <34366990+MOLLY12138@users.noreply.github.com>
2018-12-18 16:54:52 +0800

    Add files via upload

---
+++ HelloWorld/HelloWorld/SeconViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SeconViewController.swift
+//  HelloWorld
+//
+//  Created by student on 2018/10/18.
+//  Copyright © 2018年 molinjun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class SecondViewController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor.green
+        
+        
+    }
+}
+
+
+
+
+
---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by student on 2018/10/17.
+//  Copyright © 2018年 molinjun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        view.backgroundColor = UIColor.yellow
+        let label = UILabel(frame: CGRect(x: 200,y: 200,width: 200,height: 200))
+        
+        label.text = "Hello World!"
+        label.backgroundColor = UIColor.blue
+        label.textAlignment = .center
+        label.center = view.center
+        view.addSubview(label)
+        
+        let button = UIButton(frame: CGRect(x: 100, y: 200, width: 200, height: 20))
+        button.setTitle("Click me", for: .normal)
+       // button.setTitle("I am focouse", for: .highlighted)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        view.addSubview(button)
+
+        button.addTarget(self, action: #selector(btnClicked), for: .touchUpInside)
+//
+//        self.title = "first"
+//        print("viewdidload")
+    }
+
+    @IBAction func btnClicked(){
+        if let label = view.subviews.first as? UILabel{
+            label.text = "I am Clicked!"
+        }
+//
+//        self.navigationController?.pushViewController(SecondViewController(), animated: true)
+   }
+//
+    
+    
+
+}
+
